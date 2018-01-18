a5af6d7ea1bd90eaddfc760fc2f3ca26f3c920fe
WANG WENPEI <wangwenpei@nextoa.com>
2018-01-19 01:11:20 +0800

    修正模板错误

--- old-fashion/config/cabric/web/fabfile.py
+++ old-fashion/config/cabric/web/fabfile.py
@@ -6,6 +6,6 @@ from fabric.api import *
 @task
 def sync_db():
     with cd("/home/web-___pkg___/___REPO___"):
-        run("python db.py upgrade", user="web-__pkg__")
+        run("python db.py upgrade", user="web-___pkg___")
         pass
     pass
