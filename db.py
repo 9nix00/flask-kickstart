1cc038dcfc0059b4d8818e4885060751f2864ab3
WANG WENPEI <wangwenpei@nextoa.com>
2018-01-19 12:48:41 +0800

    更新db.py，添加新功能

--- db.py
+++ db.py
@@ -11,7 +11,11 @@ import sys
 
 def migrate(actions):
     """
-    指定启动文件，并从启动文件中解析可能模块
+    指定启动文件，并从启动文件中解析可能模块。
+
+    附加操作，因为此处我们会解析出模块，所以我们顺便
+    把requirements文件也一并解析
+    同样，我们强依赖顺序，对于顺序不一致出现的多个重复的问题，我们不关心
     """
 
     if not actions:
@@ -29,10 +33,21 @@ def migrate(actions):
         buffer = fp.read()
         pass
 
-    regxp = r'from ([a-z]+)\.app import'
+    regxp = r'^from ([a-z]+)\.app import'
     modules = re.findall(regxp, buffer, re.MULTILINE | re.DOTALL)
 
     root = os.path.expanduser('~/Codes/1024/hive')
+
+    project_requirements_file = os.path.join(abs_path, 'requirements.txt')
+    depends_packages = []
+
+    if 'requirements' in actions:
+        hive_requirements_file = os.path.join(root, 'requirements.txt')
+        if os.path.exists(hive_requirements_file):
+            with open(hive_requirements_file, 'r') as fp:
+                depends_packages += fp.readlines()
+                pass
+            pass
 
     for mod in modules:
         path = os.path.join(root, mod)
@@ -51,10 +66,27 @@ def migrate(actions):
             os.system('flask db migrate')
             pass
 
+        if 'requirements' in actions:
+            requirements_file = os.path.join(path, 'requirements.txt')
+
+            if os.path.exists(requirements_file):
+                with open(requirements_file, 'r') as fp:
+                    depends_packages += fp.readlines()
+                    pass
+                pass
+            pass
 
         if 'upgrade' in actions:
             os.system('flask db upgrade')
             pass
+
+        pass
+
+    with open(project_requirements_file, 'w') as fp:
+        fp.write(''.join(set(depends_packages)))
+        os.chdir(abs_path)
+        print("git commit requirements.txt -m "
+              "'sync requirements from hive.'")
         pass
 
     pass
@@ -68,6 +100,9 @@ if __name__ == "__main__":
 
     if 'migrate' in sys.argv:
         actions.append('migrate')
+
+    if 'requirements' in sys.argv:
+        actions.append('requirements')
 
     if 'upgrade' in sys.argv:
         actions.append('upgrade')
