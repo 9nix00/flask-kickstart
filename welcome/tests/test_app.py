af2a822aa1bbd30b293ba6fb6b7d4882f7ca9301
Stormxx <stormxx@1024.engineer>
2018-08-02 23:54:24 +0800

    添加注释

--- welcome/tests/test_app.py
+++ welcome/tests/test_app.py
@@ -1,6 +1,6 @@
 """
 =============
-
+测试用例
 =============
 """
 
@@ -8,8 +8,8 @@ from flask import url_for
 
 
 def test_hello(client):
-    response = client.get(url_for('welcome.hello'))
-    assert 200 == response.status_code
-    assert 'hello' in response.json.keys()
-    assert 'world' == response.json['hello']
+    response = client.get(url_for('welcome.hello'))   # 创建请求
+    assert 200 == response.status_code                # 比对http status code
+    assert 'hello' in response.json.keys()            # 检查key是否存在
+    assert 'world' == response.json['hello']          # 检查值是否正确
     pass
