1e8e7d73d3a4374c414c494895785aacd9a730ad
Stormxx <stormxx@1024.engineer>
2018-08-05 14:19:58 +0800

    enable code coverage

--- welcome/tests/conftest.py
+++ welcome/tests/conftest.py
@@ -4,9 +4,10 @@
 ====================
 """
 
-pytest_plugins = "app_fixtures.myapp",           # 固定声明，引入扩展
+pytest_plugins = "app_fixtures.myapp",  # 固定声明，引入扩展
+
 
 def pytest_namespace():
     return {
-        'entry_app': 'welcome'                             # 指定入口，通常是包名
+        'entry_app': 'welcome'  # 指定入口，通常是包名
     }
